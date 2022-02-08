dde7d47dbdf35e9636c6c83a9be02794f72d4101
Jhon Pablo <jhonpablo992@gmail.com>
2022-02-08 07:09:22 -0500

    form_post

---
+++ receber/pg_receber.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    print "Usuario: ".$usuario."<br>";
+    print "Senha: ".$senha."<br>";
+    
+
+    ?>
+</body>
+</html>
